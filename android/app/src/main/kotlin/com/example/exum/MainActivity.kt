d1c274fac61aeaf0498479599d3eb4d4a8b98c1b
luixfds <l.fds@yahoo.com>
2022-02-10 10:34:08 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/exum/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.exum
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(AppLC());
+      
+}
+class AppLC extends StatefulWidget {
+  const AppLC({Key? key}) : super(key: key);
+
+  @override
+  _AppLCState createState() => _AppLCState();
+}
+
+class _AppLCState extends State<AppLC> {
+  int num = 0;
+  int? num2;
+
+  void entrou() {
+    setState(() {
+      num++;
+    });
+  }
+  void saiu() {
+    setState(() {
+      num--;
+    });
+  }
+  void refresco() {
+    setState(() {
+      num = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          shadowColor: Colors.grey,
+          // centerTitle: true,
+          backgroundColor: Colors.pink,
+          actions: [
+            IconButton(
+                onPressed: refresco,
+                padding: const EdgeInsets.only(right: 30),
+                icon: const Icon(Icons.refresh, size:30,))
+          ],
+          title: const Text('Controle de Pessoas'),
+        ),
+
+
+        body: Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/img/wallpaper/nekuz.jpg'),
+              fit: BoxFit.cover
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+                Text(num == 20 ? 'Não pode entrar': 'Pode entrar', style: TextStyle(fontSize: 35),),
+              Padding(
+                padding: const EdgeInsets.all(20),
+                child: Text('$num', style: const TextStyle(fontSize: 50)),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                      style: TextButton.styleFrom(
+                          fixedSize: Size(100, 40),
+                          backgroundColor: num != 20 ?  Colors.black: Colors.grey,
+                          elevation: 10,
+                          primary: Colors.white,
+                          shape:RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          )
+                      ),
+                      onPressed: num != 20 ?entrou: null,
+                      child: Text('entrar',
+                      style: TextStyle(
+                          color: num != 0 ? Colors.white: Colors.white.withOpacity(0.8)),
+                      )),
+
+                  const SizedBox(width: 20,),
+
+                  TextButton(
+                      style: TextButton.styleFrom(
+                        fixedSize: const Size(100, 40),
+                        backgroundColor: num != 0 ? Colors.black: Colors.grey,
+                        elevation: 10,
+                        primary: Colors.white,
+                        shape:RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        )
+                      ),
+                      onPressed: num != 0 ?saiu: null,
+                      child: Text('sair',
+                        style: TextStyle(
+                            color: num != 0 ? Colors.white: Colors.white.withOpacity(0.8)),)),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
